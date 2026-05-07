9e53f17bcc3cb1645705297578a475cfe6e2d94d
unknown <maint@PORTA-CLIN.ch-bry.org>
2026-05-07 15:43:18 +0200

    restore: clerk middleware

--- maison-pomeranian/middleware.ts
+++ maison-pomeranian/middleware.ts
@@ -1,5 +1,19 @@
-export default function middleware() {}
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
+
+const isProtectedRoute = createRouteMatcher([
+  '/le-cercle(.*)',
+  '/api/dogs(.*)',
+]);
+
+export default clerkMiddleware(async (auth, req) => {
+  if (isProtectedRoute(req)) {
+    await auth.protect();
+  }
+});
 
 export const config = {
-  matcher: [],
+  matcher: [
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    '/(api|trpc)(.*)',
+  ],
 };
