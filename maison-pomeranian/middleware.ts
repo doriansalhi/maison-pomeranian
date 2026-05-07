cf2c9ff35b76a1fe5605bbea2618b846976fedb0
unknown <maint@PORTA-CLIN.ch-bry.org>
2026-05-07 14:47:55 +0200

    fix: disable clerk middleware temporarily

--- maison-pomeranian/middleware.ts
+++ maison-pomeranian/middleware.ts
@@ -1,21 +1,5 @@
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
-
-const isProtectedRoute = createRouteMatcher([
-  '/le-cercle(.*)',
-  '/api/dogs(.*)',
-]);
-
-export default clerkMiddleware(async (auth, req) => {
-  if (isProtectedRoute(req)) {
-    await auth.protect();
-  }
-});
+export default function middleware() {}
 
 export const config = {
-  matcher: [
-    // Skip Next.js internals and all static files
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
-    '/(api|trpc)(.*)',
-  ],
+  matcher: [],
 };
