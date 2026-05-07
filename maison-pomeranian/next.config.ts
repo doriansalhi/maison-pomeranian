e231a0acf18b9c1f1fdc8cc30a4b2e5ea0db86a4
unknown <maint@PORTA-CLIN.ch-bry.org>
2026-05-07 15:40:59 +0200

    fix: remove redirect loop

--- maison-pomeranian/next.config.ts
+++ maison-pomeranian/next.config.ts
@@ -2,16 +2,6 @@ import type { NextConfig } from 'next';
 
 const config: NextConfig = {
   reactStrictMode: true,
-  async redirects() {
-    return [
-      {
-        source: '/:path*',
-        has: [{ type: 'host', value: 'www.maison-pomeranian.com' }],
-        destination: 'https://maison-pomeranian.com/:path*',
-        permanent: true, // 301
-      },
-    ];
-  },
   images: {
     remotePatterns: [
       {
